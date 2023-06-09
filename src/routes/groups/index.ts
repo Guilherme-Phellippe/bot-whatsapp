285ecfb0b26af9fa662f5fae99f78a5f58f86e05
guilher-phellippe <guilhermephellippe@outlook.com>
2023-06-09 16:47:43 -0300

    new struture code

--- src/routes/groups/index.ts
+++ src/routes/groups/index.ts
@@ -5,33 +5,36 @@ import { configDotenv } from 'dotenv'
 configDotenv();
 const app = Router();
 
-// var clientWhatsapp:Promise<Whatsapp> = create({
-//     session: "tem-sabor-message",
-//     disableWelcome: true,
-// })
+const createClientWhatsapp = async (): Promise<Whatsapp> => {
+    return create({
+        session: "tem-sabor-message",
+        disableWelcome: true,
+    });
+};
 
-app.get('/', (req: any, res: any) => {
-    res.send("/foi")
+
+app.post('/send-recipe', async (req: any, res: any) => {
+    const { url, name, description, link } = req.body;
+    const id_group = process.env.ID_GROUPS || ""
+
+    try {
+        const client: Whatsapp = await createClientWhatsapp();
+
+        await client.sendImage(
+            id_group,
+            url,
+            name,
+            `*${name.toUpperCase()}*\n\n${description}\n\nAcesse o link abaixo👇👇\n\n${link}\n\n_caso o link não esteja disponível, salve esse número de contato, feche e abra o whatsapp novamente! informe-nos se resolveu o problema._`
+        ).then((response) => {
+            res.status(200).json(response)
+        }).catch(async (error) => {
+            console.log(error)
+            await client.sendText("553592267960@c.us", "Erro ao enviar receita ao grupo")
+        });
+    } catch (error) {
+
+    }
 })
-
-// app.post('/send-recipe', async (req: any, res: any) => {
-//     const { url, name, description, link } = req.body;
-//     const id_group = process.env.ID_GROUPS || ""
-
-//     clientWhatsapp.then((client: Whatsapp)=> {
-//         client.sendImage(
-//             id_group,
-//             url,
-//             name,
-//             `*${name.toUpperCase()}*\n\n${description}\n\nAcesse o link abaixo👇👇\n\n${link}\n\n_caso o link não esteja disponível, salve esse número de contato, feche e abra o whatsapp novamente! informe-nos se resolveu o problema._`
-//         ).then((response) => {
-//             res.status(200).json(response)
-//         }).catch(async (error) => {
-//             console.log(error)
-//             await client.sendText("553592267960@c.us", "Erro ao enviar receita ao grupo")
-//         });
-//     }).catch(err => console.log(err))
-// })
 
 
 export default app
