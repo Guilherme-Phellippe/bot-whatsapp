c2ee673184c4612941f7efd6aedcf00ade08c79a
guilher-phellippe <guilhermephellippe@outlook.com>
2023-06-09 15:34:56 -0300

    change name package.json

--- build/index.js
+++ build/index.js
@@ -5,6 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router_1 = __importDefault(require("./router"));
+const cors_1 = __importDefault(require("cors"));
 const server = (0, express_1.default)();
+server.use((0, cors_1.default)({
+    origin: ["https://temsabor.blog", "http://localhost:3000", "http://127.0.0.0:3000/"]
+}));
+server.use(express_1.default.json());
 server.use(router_1.default);
 server.listen(8081, () => console.log("SERVER IS RUNNING ON PORT 8081"));
--- build/routes/groups/index.js
+++ build/routes/groups/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const config_venom_1 = require("../../config/config-venom");
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.configDotenv)();
 const app = (0, express_1.Router)();
@@ -8,19 +9,15 @@ app.get('/', (req, res) => {
     res.send("ooi");
 });
 app.post('/send-recipe', async (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-    // const { url, name, description, link } = req.body;
-    // const id_group = process.env.ID_GROUPS || ""
-    // client.then((client: Whatsapp) => {
-    //     client.sendImage(
-    //         id_group,
-    //         url,
-    //         name,
-    //         `*${name.toUpperCase()}*\n\n${description}\n\nAcesse o link abaixo👇👇\n\n${link}\n\n_caso o link não esteja disponível, salve esse número de contato, feche e abra o whatsapp novamente! informe-nos se resolveu o problema._`
-    //     ).then((response) => {
-    //         res.status(200).json(response)
-    //     }).catch((error) => console.log(error));
-    // })
+    const { url, name, description, link } = req.body;
+    const id_group = process.env.ID_GROUPS || "";
+    config_venom_1.client.then((client) => {
+        client.sendImage(id_group, url, name, `*${name.toUpperCase()}*\n\n${description}\n\nAcesse o link abaixo👇👇\n\n${link}\n\n_caso o link não esteja disponível, salve esse número de contato, feche e abra o whatsapp novamente! informe-nos se resolveu o problema._`).then((response) => {
+            res.status(200).json(response);
+        }).catch(async (error) => {
+            console.log(error);
+            await client.sendText("553592267960@c.us", "Erro ao enviar receita ao grupo");
+        });
+    });
 });
 exports.default = app;
